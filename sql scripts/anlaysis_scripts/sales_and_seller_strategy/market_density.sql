b6db0d2e31434a5bb81d142a14f5aa1b3eb0f7de
Sebastian Hopgood <sebastianhopgood@gmail.com>
2026-04-17 17:33:21 -0400

    Add files via upload

---
+++ sql scripts/anlaysis_scripts/sales_and_seller_strategy/market_density.sql
@@ -0,0 +1,13 @@
+SELECT 
+  c.customer_state,
+  
+  -- Compare customer demand vs. local seller supply by state
+  COUNT(DISTINCT c.customer_id) AS customer_count,
+
+  -- Measure supply: Total unique sellers located in the region
+  COUNT(DISTINCT s.seller_id) AS seller_count
+FROM `olist-360-e-commerce.analytical_data.dim_customers` AS c
+
+-- LEFT JOIN ensures we see states with customers even if they have zero sellers
+LEFT JOIN `olist-360-e-commerce.analytical_data.dim_sellers` AS s ON c.customer_state = s.seller_state
+GROUP BY 1;
